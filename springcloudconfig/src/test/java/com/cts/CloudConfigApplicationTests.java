0cfa3d288038d37f3bfe533b1178ad9ca87060f9
elumalais123 <elumalai.subramaniyan@gmail.com>
2024-06-21 15:02:14 +0530

    first commit two

---
+++ springcloudconfig/src/test/java/com/cts/CloudConfigApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cts;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
